2fa947276f301bc8cbaefc16a438abb29e9e20cc
PoisonedMinds <steven_biro@hotmail.com>
2015-02-20 12:30:22 -0500

    prime numbers

---
+++ Resistors/src/resistors/Resistors.java
@@ -0,0 +1,18 @@
+
+
+package resistors;
+import javax.swing.*;
+/**
+ * @title Resistors
+ * @author Steven Biro
+ * @teacher Mr. J. Carron
+ * @date 20-Feb-2015 12:29:51 PM
+ * @purpose The purpose of this program is to
+ */
+public class Resistors {
+
+    public static void main(String[] args) {
+        
+    }
+
+}
