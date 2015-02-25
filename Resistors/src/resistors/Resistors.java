006596b1b9239f974f7f7b9ccd30565e5213fa2e
PoisonedMinds <steven_biro@hotmail.com>
2015-02-25 15:06:16 -0500

    prime numbers

--- Resistors/src/resistors/Resistors.java
+++ Resistors/src/resistors/Resistors.java
@@ -8,7 +8,7 @@ import javax.swing.*;
  * @author Steven Biro
  * @teacher Mr. J. Carron
  * @date 20-Feb-2015 12:29:51 PM
- * @purpose The purpose of this program is to
+ * @purpose The purpose of this program is to calculate the value of resistance a resistor provides
  */
 public class Resistors {
 
