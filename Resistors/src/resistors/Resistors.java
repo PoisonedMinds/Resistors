9b67084cc7be8f942d494eb3acd81558bcdbbb7a
PoisonedMinds <steven_biro@hotmail.com>
2015-02-25 10:54:09 -0500

    prime numbers

--- Resistors/src/resistors/Resistors.java
+++ Resistors/src/resistors/Resistors.java
@@ -15,8 +15,42 @@ public class Resistors {
        String resistors, colours[]=new String[3];
        int one,two,three;
        resistors=JOptionPane.showInputDialog("Input a combonation of 3 colours to find their equivlent resistance. Ex. Red-Blue-Black");
-       colours=resistors.split("-");
-       
+       colours=resistors.toLowerCase().split("-");
+       for (int i=0;i<colours.length;i++) {
+           if (colours[i].equals("black")) {
+               colours[i]="0";
+           } else if (colours[i].equals("brown")) {
+               colours[i]="0";
+           } else if (colours[i].equals("red")) {
+               colours[i]="0";
+           } else if (colours[i].equals("orange")) {
+               colours[i]="0";
+           } else if (colours[i].equals("yellow")) {
+               colours[i]="0";
+           } else if (colours[i].equals("green")) {
+               colours[i]="0";
+           } else if (colours[i].equals("blue")) {
+               colours[i]="0";
+           } else if (colours[i].equals("violet")) {
+               colours[i]="0";
+           } else if (colours[i].equals("grey")) {
+               colours[i]="0";
+           } else if (colours[i].equals("white")) {
+               colours[i]="0";
+           } else {
+               JOptionPane.showMessageDialog(null, "invlid colour");
+               System.exit(1);
+           }
+           
+           
+           
+           
+           
+           
+           
+           
+           
+       }
        
         
         
